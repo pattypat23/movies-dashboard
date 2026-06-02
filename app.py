606b2605d465e51b8d8dad4e56fdf713b6ac3d2b
pattypat23 <opmm23@gmail.com>
2026-06-02 15:36:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
 import pandas as pd
 import firebase_admin
+from firebase_admin import credentials, firestore
+# -------------------------
+# CONEXIÓN A FIREBASE
+# -------------------------
+if not firebase_admin._apps:
+    cred = credentials.Certificate("movies-dashboard-90974-firebase-adminsdk.json")
+    firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-from firebase_admin import credentials
-from firebase_admin import firestore
+# -------------------------
+# FUNCION PARA CARGAR DATOS
+# -------------------------
+
 st.title("Mi app de películas")
 
 def load_movies():
@@ -13,14 +23,20 @@ def load_movies():
      movies.append(doc.to_dict())
     return pd.DataFrame(movies)
     movies_df = load_movies()
+
+# -------------------------
+# UI STREAMLIT
+# -------------------------
     movies_df.head()
     st.title("Movies Dashboard")
+
+# Mostrar todos
 if st.sidebar.checkbox("Mostrar todos los filmes"):
     st.header("Listado completo de filmes")
     st.dataframe(movies_df)
     st.sidebar.subheader("Buscar película")
     movie_name = st.sidebar.text_input("Ingrese parte del título")
-
+# Buscar
 if st.sidebar.button("Buscar"):
     result = movies_df[movies_df["name"].str.contains(
     movie_name,
